87306dc89c77c22767fa4e7f85770203244ee9ff
Milos Subotic <milos.subotic.sm@gmail.com>
2020-05-19 17:09:34 +0200

    Digits.

--- images/gen_7segm_digits.jl
+++ images/gen_7segm_digits.jl
@@ -1,5 +1,8 @@
 #!/usr/bin/env julia
 
+background_color = "black"
+#background_color = "white" #TODO For debug.
+
 function gen_digit(color_name, color, digit)
 	file_name = "$(color_name)_$digit"
 	open("$file_name.svg", "w") do of
@@ -7,20 +10,51 @@ function gen_digit(color_name, color, digit)
 		p("<?xml version=\"1.0\" encoding=\"UTF-8\"?>")
 		p("<!DOCTYPE svg>")
 		p("<svg version=\"1.1\" width=\"32\" height=\"64\">")
-		p("<path fill=\"$color\" d=\"M5 3  L8 0  L8 6  Z\"/>")
+		l(d) = p("<path fill=\"$color\" d=\"$d\"/>")
+		if digit in [0, 2, 3, 5, 6, 7, 8, 9, 10, 12, 14, 15] # segm a
+			l("M5 3  L8 0  L23 0  L26 3  L23 6  L8 6  Z")
+		end
+		if digit in [0, 1, 2, 3, 4, 7, 8, 9, 10, 13] # segm b
+			l("M28 5  L31 8  L31 27  L28 30  L25 27  L25 8  Z")
+		end
+		if digit in [0, 1, 3, 4, 5, 6, 7, 8, 9, 10, 11, 13] # segm c
+			l("M28 57  L31 54  L31 35  L28 32  L25 35  L25 54  Z")
+		end
+		if digit in [0, 2, 3, 5, 6, 8, 9, 11, 12, 13, 14] # segm d
+			l("M5 59  L8 62  L23 62  L26 59  L23 56  L8 56  Z")
+		end
+		if digit in [0, 2, 6, 8, 10, 11, 12, 13, 14, 15] # segm e
+			l("M3 57  L6 54  L6 35  L3 32  L0 35  L0 54  Z")
+		end
+		if digit in [0, 4, 5, 6, 8, 9, 10, 11, 12, 14, 15] # segm f
+			l("M3 5  L6 8  L6 27  L3 30  L0 27  L0 8  Z")
+		end
+		if digit in [2, 3, 4, 5, 6, 8, 9, 10, 11, 13, 14, 15] # segm g
+			l("M5 31  L8 28  L23 28  L26 31  L23 34  L8 34  Z")
+		end
 		p("</svg>")
 	end
 	
-	run(`inkscape -z -w 32 -h 64 -b black $file_name.svg -e $file_name.png`)
-	run(`eog $file_name.png`)
+	run(Cmd([
+		"inkscape",
+		"--without-gui",
+		"--export-width=32",
+		"--export-height=64",
+		"--export-background=$background_color",
+		"$file_name.svg",
+		"--export-png=$file_name.png",
+	]))
+	run(`rm $file_name.svg`)
+	#run(`eog $file_name.png`)
 	
 	return file_name
 end
 
 function gen_digit_set(color_name, color)
-	for d in 0:0
+	for d in 0:15
 		gen_digit(color_name, color, d)
 	end
 end
 
 gen_digit_set("green", "#00CC00")
+gen_digit_set("red", "#FF0000")
